2a7b35ba1b02cf6e2e2b5c28a04a09211b07a5ab
wflove520 <1816843562@qq.com>
2024-07-24 21:38:21 +0800

    Update rehanwuda.js

--- rehanwuda.js
+++ rehanwuda.js
@@ -1,7 +1,9 @@
 [rewrite_local]
 
-^https?://dancefit.dailyyogac.com/dancefit/user/common/info("?.*)? url script-response-body https://github.com/wflove520/qx/raw/main/rehanwudao.js
-^https?://dancefit.dailyyogac.com/dancefit/user/obprogram/detail("?.*)? url script-response-body https://github.com/wflove520/qx/raw/main/rehanwudao.js
+https://dancefit.dailyyogac.com/dancefit/user/common/info?df_channel=800001&df_deviceid=a3650933e9014668ab0bf75717a67248&df_devicetype=2&df_is_gray=0&df_is_pad=0&df_version=df-1.29.1.0&sid=ca6b586a545c43c40d6babcf931e7ea5&sign=0e92d0a322e1c9ce18138e29e3253ea1&time=1721825133&uid=5013873647
+ url script-response-body https://github.com/wflove520/qx/raw/main/rehanwudao.js
+https://dancefit.dailyyogac.com/dancefit/user/obprogram/detail?df_channel=800001&df_deviceid=a3650933e9014668ab0bf75717a67248&df_devicetype=2&df_is_gray=0&df_is_pad=0&df_version=df-1.29.1.0&sid=ca6b586a545c43c40d6babcf931e7ea5&sign=78f525e1cd3b4be724e2f6f69542044f&time=1721825128&uid=5013873647
+ url script-response-body https://github.com/wflove520/qx/raw/main/rehanwudao.js
 
 [mitm]
 hostname = dancefit.dailyyogac.com
